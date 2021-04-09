c9f75f4cc080900808a59f0eeb549d634b50394e
andy roberts <andyrob@microsoft.com>
2021-04-09 16:27:24 -0400

    Update: Challenge-1.md Challenge-2.md Challenge-3.md Challenge-4.md Challenge-5.md Challenge-6.md Challenge-7.md Challenge-8.md LICENSE README.md basic_report.pbix dw_ddl.sql notebooks synapse

--- notebooks/Users/andyrob@microsoft.com/andyrob-notebook.py
+++ notebooks/Users/andyrob@microsoft.com/andyrob-notebook.py
@@ -1,12 +1,63 @@
 # Databricks notebook source
-configs = {"fs.azure.account.auth.type": "OAuth",
-       "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
-       "fs.azure.account.oauth2.client.id": "<appId>",
-       "fs.azure.account.oauth2.client.secret": "<clientSecret>",
-       "fs.azure.account.oauth2.client.endpoint": "https://login.microsoftonline.com/<tenant>/oauth2/token",
-       "fs.azure.createRemoteFileSystemDuringInitialization": "true"}
+# If mounting with OAth
+
+# configs = {"fs.azure.account.auth.type": "OAuth",
+#        "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
+#        "fs.azure.account.oauth2.client.id": "<appId>",
+#        "fs.azure.account.oauth2.client.secret": "<clientSecret>",
+#        "fs.azure.account.oauth2.client.endpoint": "https://login.microsoftonline.com/<tenant>/oauth2/token",
+#        "fs.azure.createRemoteFileSystemDuringInitialization": "true"0}
+# 
+# dbutils.fs.mount(
+# source = "abfss://<container-name>@<storage-account-name>.dfs.core.windows.net/folder1",
+# mount_point = "/mnt/flightdata",
+# extra_configs = configs)
+
+# COMMAND ----------
 
 dbutils.fs.mount(
-source = "abfss://<container-name>@<storage-account-name>.dfs.core.windows.net/folder1",
-mount_point = "/mnt/flightdata",
-extra_configs = configs)
+  source = "wasbs://datalake@andyrobretailstor.blob.core.windows.net/andyrob",
+  mount_point = "/mnt/datalake",
+  extra_configs = {"fs.azure.account.key.andyrobretailstor.blob.core.windows.net":dbutils.secrets.get(scope = "datalake", key = "datalakeStorageAccountKey")})
+
+
+
+
+# COMMAND ----------
+
+# MAGIC %fs ls dbfs:/mnt/datalake/raw
+
+# COMMAND ----------
+
+dfCustomer = spark.read.option("header",True).option("inferSchema",True).csv("dbfs:/mnt/datalake/raw/RetailCustomer.csv")
+dfProduct = spark.read.option("header",True).option("inferSchema",True).csv("dbfs:/mnt/datalake/raw/RetailProducts.csv")
+dfStore = spark.read.option("header",True).option("inferSchema",True).csv("dbfs:/mnt/datalake/raw/RetailStores.csv")
+dfTxnHeader = spark.read.option("header",True).option("inferSchema",True).csv("dbfs:/mnt/datalake/raw/RetailTransactionHeader.csv")
+dfTxnDetail = spark.read.option("header",True).option("inferSchema",True).csv("dbfs:/mnt/datalake/raw/RetailTransactionDetails.csv")
+
+
+# COMMAND ----------
+
+dfTxnDetail.printSchema()
+dfTxnDetail.head(10)
+
+
+# COMMAND ----------
+
+import pyspark.sql.functions as f
+
+dfAggregate = dfTxnDetail \
+.join(dfTxnHeader, dfTxnDetail.TransactionHeaderID == dfTxnHeader.TransactionHeaderID, "inner") \
+.join(dfStore, dfTxnHeader.StoreRetailID == dfStore.StoreRetailID, "inner") \
+.withColumn("year", f.year(dfTxnHeader.Date.cast("date"))) \
+.withColumn("month", f.month(dfTxnHeader.Date.cast("date"))) \
+.groupBy("year", "month", dfStore.StoreRetailID).agg(f.sum("TotalAmount").alias("TotalAmount"), f.sum("ShippingCost").alias("ShippingCost"), f.first("Name").alias("StoreName"))
+
+
+
+# COMMAND ----------
+
+dfAggregate.coalesce(1).repartition(1).write.option("header", "true").csv("/mnt/datalake/processed/StoreSalesByMonthSpark2.csv")
+
+# COMMAND ----------
+
